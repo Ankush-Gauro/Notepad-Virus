a28e06ba884fbec43bc932844f297a285dfb5f9f
AnkushGauro <72354882+Ankush-Gauro@users.noreply.github.com>
2021-01-19 21:27:15 +0545

    Update love.bat

--- Notepad virus/love.bat
+++ Notepad virus/love.bat
@@ -5,13 +5,10 @@ set /p love=
 if %love%==yes goto love
 id %love%==no goto hate
 :love
-echo I love you too...
-echo Developed by Ankush Gauro Comp-B
+echo Thanks
 pause
 :hate
 echo Lol... but i love you hehehehehe
-echo Developed by Ankush Gauro Comp-B
-echo Hacked...
 echo Crashing in 10 seconds...
 timeout 10
 shutdown -s -t 100
